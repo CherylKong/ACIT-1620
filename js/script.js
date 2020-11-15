882c6acd2d73253103749cfba6be7f0a77ce3464
Cheryl Kong <ykong13@my.bcit.ca>
2020-11-14 22:28:27 -0800

    check if user input same as course code

--- js/script.js
+++ js/script.js
@@ -21,3 +21,21 @@ do
     num  = prompt("Enter 4-digit number: ")  
 }
 while (!Number(num))
+
+let b = 'False'
+for (let x of courselist)
+  {
+    if(x.code.includes(num))
+      {
+        b='True';
+        console.log(`Yes I am taking the course: ${x.code} - ${x.name}`);
+
+      }
+  }
+
+if (b == 'False')
+  {
+    let new_course = {code: num, name: 'null'}
+    courselist.push(new_course);
+    console.log(`added a new course code: ${num}`);   
+  }
