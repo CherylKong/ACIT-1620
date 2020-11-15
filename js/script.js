05219061ad7415eb5c93799ff71f60d1ee15c72f
Cheryl Kong <ykong13@my.bcit.ca>
2020-11-14 20:52:24 -0800

    create array and add 3 courses code and name

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let course1 = {
+    code: "ACIT 1620",
+    name: "Web Fundamental Technologies"
+};
+
+let course2 = {
+    code: "ACIT 1420",
+    name: "Introduction to Systems Administration",
+}
+ 
+let course3 = {
+    code: "ACIT 1515",
+    name: "Scripting for IT",
+}
+ 
+let courselist = [course1, course2, course3];
