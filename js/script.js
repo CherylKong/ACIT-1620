a30243d6175e1ce438c67f8632b4c1bf4f2f6140
Cheryl Kong <ykong13@my.bcit.ca>
2020-11-22 16:56:42 -0800

    create function that check if the user input is in the course

--- js/script.js
+++ js/script.js
@@ -1,68 +1,72 @@
 let course1 = {
-    code: "ACIT 1620",
-    name: "Web Fundamental Technologies"
+  code: "ACIT 1620",
+  name: "Web Fundamental Technologies"
 };
 
 let course2 = {
-    code: "ACIT 1420",
-    name: "Introduction to Systems Administration",
+  code: "ACIT 1420",
+  name: "Introduction to Systems Administration",
 }
- 
+
 let course3 = {
-    code: "ACIT 1515",
-    name: "Scripting for IT",
+  code: "ACIT 1515",
+  name: "Scripting for IT",
 }
- 
+
 let courselist = [course1, course2, course3];
 
-// let num;
-// do
-// {
-//   num  = prompt("Enter 4-digit number: ")
-//   while (num.length != 4)
-//   {
-//     num  = prompt("Enter 4-digit number: ")
-//   }
-    
-// }
-// while (!Number(num))
+function promt_user() {
+  let num;
+  do {
+    num = prompt("Enter 4-digit number: ")
+    while (num.length != 4) {
+      num = prompt("Enter 4-digit number: ")
+    }
 
-// let b = 'False'
-// for (let x of courselist)
-//   {
-//     if(x.code.includes(num))
-//       {
-//         b='True';
-//         console.log(`Yes I am taking the course: ${x.code} - ${x.name}`);
+  }
+  while (!Number(num))
 
-//       }
-//   }
+  return num
+}
 
-// if (b == 'False')
-//   {
-//     let new_course = {code: num, name: 'null'}
-//     courselist.push(new_course);
-//     console.log(`added a new course code: ${num}`);   
-//   }
-
-function createCourseArray()
-{
+function createCourseArray() {
   let course_list = []
   code = document.querySelectorAll("a.left_side")
   date = document.querySelectorAll("p.left_side")
-  for (let x = 0; x<code.length;x++)
-  {
+  for (let x = 0; x < code.length; x++) {
     let course;
-    course = {code: code[x].innerText, date: date[x].innerText}
+    course = { code: code[x].innerText, date: date[x].innerText }
     course_list.push(course)
   }
-  console.log(course_list)
+  return course_list
 }
 
-// function findCourse(course_list)
-// {
-
-// }
+function findCourse(course_list) {
+  num = promt_user()
+  course = document.querySelectorAll('section')
+  let valid = 'false'
+  for (let y of course) {
+    if (y.innerText.includes(num)) {
+      valid = 'true'
+      y.style.backgroundColor = 'green'
+    }
+  }
+  if(valid === 'false') 
+  {
+    let all_course =document.querySelector('main')
+    let new_course = document.createElement("section")
+    let new_code = document.createTextNode(num)
+    let new_date = document.createTextNode('Fall 2020')
+    let new_descp = document.createTextNode('N/A')
+    new_course.appendChild(new_code)
+    new_course.appendChild(new_date)
+    new_course.appendChild(new_descp)
+    new_course.setAttribute("class","last_course")
+    
+    all_course.appendChild(new_course)
+    
+  }
+}
 
 createCourseArray()
-// findCourse(course_list)
+findCourse(createCourseArray())
